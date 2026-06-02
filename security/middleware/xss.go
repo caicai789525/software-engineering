3165d1224c29d429d8b768a079007507d4972294
蔡宗谚 <kevin13537412565@163.com>
2026-06-02 17:00:46 +0800

    Revert "问题ID: acd3b82e-56ef-493b-beeb-21b20c6d94cd #82"
    This reverts commit 3af9ecde25e4338b7673be817294a5da3e29a69e.

--- security/middleware/xss.go
+++ security/middleware/xss.go
@@ -11,85 +11,49 @@ import (
 
 func XSSFilter() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		setSecurityHeaders(c)
-		filterQueryParams(c)
-		filterRequestBody(c)
+		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
+		c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
+		c.Writer.Header().Set("X-Frame-Options", "DENY")
+
+		for key, values := range c.Request.URL.Query() {
+			for i, value := range values {
+				values[i] = escapeXSS(value)
+			}
+			c.Request.URL.Query()[key] = values
+		}
+
+		if c.Request.Method == "POST" || c.Request.Method == "PUT" || c.Request.Method == "PATCH" {
+			contentType := c.GetHeader("Content-Type")
+			if strings.Contains(contentType, "application/json") {
+				bodyBytes, err := io.ReadAll(c.Request.Body)
+				if err == nil {
+					escapedBody := escapeXSS(string(bodyBytes))
+					c.Request.Body = io.NopCloser(bytes.NewBufferString(escapedBody))
+				}
+			} else if strings.Contains(contentType, "application/x-www-form-urlencoded") {
+				c.Request.ParseForm()
+				for key, values := range c.Request.PostForm {
+					for i, value := range values {
+						values[i] = escapeXSS(value)
+					}
+					c.Request.PostForm[key] = values
+				}
+			}
+		}
+
 		c.Next()
 	}
 }
 
-func setSecurityHeaders(c *gin.Context) {
-	c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
-	c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
-	c.Writer.Header().Set("X-Frame-Options", "DENY")
-}
-
-func filterQueryParams(c *gin.Context) {
-	for key, values := range c.Request.URL.Query() {
-		c.Request.URL.Query()[key] = escapeStringSlice(values)
-	}
-}
-
-func filterRequestBody(c *gin.Context) {
-	if !isWriteMethod(c.Request.Method) {
-		return
-	}
-
-	contentType := c.GetHeader("Content-Type")
-	switch {
-	case strings.Contains(contentType, "application/json"):
-		filterJSONBody(c)
-	case strings.Contains(contentType, "application/x-www-form-urlencoded"):
-		filterFormBody(c)
-	}
-}
-
-func isWriteMethod(method string) bool {
-	return method == "POST" || method == "PUT" || method == "PATCH"
-}
-
-func filterJSONBody(c *gin.Context) {
-	bodyBytes, err := io.ReadAll(c.Request.Body)
-	if err != nil {
-		return
-	}
-	escapedBody := escapeXSS(string(bodyBytes))
-	c.Request.Body = io.NopCloser(bytes.NewBufferString(escapedBody))
-}
-
-func filterFormBody(c *gin.Context) {
-	c.Request.ParseForm()
-	for key, values := range c.Request.PostForm {
-		c.Request.PostForm[key] = escapeStringSlice(values)
-	}
-}
-
-func escapeStringSlice(values []string) []string {
-	for i, value := range values {
-		values[i] = escapeXSS(value)
-	}
-	return values
-}
-
 func escapeXSS(s string) string {
 	s = html.EscapeString(s)
-	s = replaceSpecialChars(s)
-	return s
-}
-
-func replaceSpecialChars(s string) string {
-	replacements := map[string]string{
-		"<":  "&lt;",
-		">":  "&gt;",
-		"\"": "&quot;",
-		"'":  "&#x27;",
-		"/":  "&#x2F;",
-		"`":  "&#x60;",
-		"=":  "&#x3D;",
-	}
-	for old, new := range replacements {
-		s = strings.ReplaceAll(s, old, new)
-	}
+	s = strings.ReplaceAll(s, "<", "&lt;")
+	s = strings.ReplaceAll(s, ">", "&gt;")
+	s = strings.ReplaceAll(s, "\"", "&quot;")
+	s = strings.ReplaceAll(s, "'", "&#x27;")
+	s = strings.ReplaceAll(s, "/", "&#x2F;")
+	s = strings.ReplaceAll(s, "`", "&#x60;")
+	s = strings.ReplaceAll(s, "=", "&#x3D;")
 	return s
 }
 
