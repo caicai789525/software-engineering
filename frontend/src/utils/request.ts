12637717ecfd27b63df25d1cac73f67ee1c81623
wen <menglinwen14@gmail.com>
2026-06-04 10:22:03 +0800

    问题ID: f6d40272-7e17-4781-98aa-c16e88fcc552 #78

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
 import { message } from 'antd'
 import { ApiResponse } from '../types'
 
